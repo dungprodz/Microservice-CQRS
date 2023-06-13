47dbc8fcee80d3924a27f8fa396bfb806fc9ac00
DungNT2 <nguyentiendunga42002@gmail.com>
2023-06-13 10:37:56 +0700

    add api gateway

--- bookservice/src/main/java/com/example/bookservice/command/controller/BookCommandController.java
+++ bookservice/src/main/java/com/example/bookservice/command/controller/BookCommandController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/book")
+@RequestMapping("api/v1/books")
 public class BookCommandController {
     private final CommandGateway commandGateway;
     @Autowired
